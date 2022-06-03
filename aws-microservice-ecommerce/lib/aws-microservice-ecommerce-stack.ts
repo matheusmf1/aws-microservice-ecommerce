15e107d8fcf64830a8dbbace79ba21dfdadc6654
Matheus Franco <matheus.franco.ext@brf.com>
2022-06-03 19:16:31 -0300

    Add DynamoDB infrastructure

--- aws-microservice-ecommerce/lib/aws-microservice-ecommerce-stack.ts
+++ aws-microservice-ecommerce/lib/aws-microservice-ecommerce-stack.ts
@@ -1,16 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb'
 
 export class AwsMicroserviceEcommerceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroserviceEcommerceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const productTable = new Table( this, "product", {
+
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+
   }
 }
