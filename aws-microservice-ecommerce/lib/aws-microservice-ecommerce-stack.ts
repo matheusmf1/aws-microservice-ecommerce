abd59ac30a9c16db42adbcd71964fa7af95a7695
Matheus <mathew.mfranco@gmail.com>
2022-06-15 12:00:01 -0300

    Implement IaC API Gateway with Lambda Integration

--- aws-microservice-ecommerce/lib/aws-microservice-ecommerce-stack.ts
+++ aws-microservice-ecommerce/lib/aws-microservice-ecommerce-stack.ts
@@ -4,11 +4,11 @@ import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb'
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsMicroserviceEcommerceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
 
     const productTable = new Table( this, "product", {
 
@@ -24,7 +24,7 @@ export class AwsMicroserviceEcommerceStack extends Stack {
 
     const productLambdaFuntion = new NodejsFunction( this, 'ProductFunction', {
 
-      entry: path.join(__dirname, './scr/product.index.js'),
+      entry: path.join(__dirname, '../src/product/index.js' ),
 
       bundling: {
         externalModules: [
@@ -45,5 +45,21 @@ export class AwsMicroserviceEcommerceStack extends Stack {
     //alow lambda to access dynamoDB Table
     productTable.grantReadWriteData( productLambdaFuntion );
 
+
+    //Product API Gateway
+    const apigtw = new LambdaRestApi( this, "productAPI", {
+      restApiName: "Product Service",
+      handler: productLambdaFuntion,
+      proxy: false
+    });
+
+    const product = apigtw.root.addResource( "product" );
+    product.addMethod( "GET" ); // GET /product
+    product.addMethod( "POST" ); // POST /product
+
+    const singleProduct = product.addResource( "{id}" ); // /product/{id}
+    singleProduct.addMethod( "GET" ) // GET /product/{id}
+    singleProduct.addMethod( "PUT" ) // PUT /product/{id}
+    singleProduct.addMethod( "DELETE" ) // DELETE /product/{id}
   }
 }
